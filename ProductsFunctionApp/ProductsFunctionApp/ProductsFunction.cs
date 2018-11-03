7a0d7b4d21057afe7aa9093acd591823ece1ecf6
Nirjal Khadka <nirjal@gmail.com>
2018-11-03 21:39:27 +0100

    Removed unsed references

--- ProductsFunctionApp/ProductsFunctionApp/ProductsFunction.cs
+++ ProductsFunctionApp/ProductsFunctionApp/ProductsFunction.cs
@@ -43,23 +43,6 @@ namespace ProductsFunctionApp
                         };
                return req.CreateResponse(HttpStatusCode.OK, result);
             }
-          
-
-            //// parse query parameter
-            //string name = req.GetQueryNameValuePairs()
-            //    .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-            //    .Value;
-
-            //if (name == null)
-            //{
-            //    // Get request body
-            //    dynamic data = await req.Content.ReadAsAsync<object>();
-            //    name = data?.name;
-            //}
-
-            //return name == null
-            //    ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-            //    : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
     }
 }
